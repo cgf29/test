f2dc7371e01173b8e12759dd28ce8833c783ba16
Your Name <vanyasv@gmail.com>
2020-07-03 18:35:04 +0300

    Updates

---
+++ precache-manifest.1d92866be1b68a18067bcab7348120e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779d9756b9a579e2794b9ab3437b5161",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "8541824a3285a48b1845",
+    "url": "/test/static/css/main.93df4443.chunk.css"
+  },
+  {
+    "revision": "6e2b4ea36bab2a895be2",
+    "url": "/test/static/js/2.84901ce9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.84901ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8541824a3285a48b1845",
+    "url": "/test/static/js/main.1129f461.chunk.js"
+  },
+  {
+    "revision": "c0c691af2b5cd6ed53d1",
+    "url": "/test/static/js/runtime-main.88497f4c.js"
+  }
+]);
