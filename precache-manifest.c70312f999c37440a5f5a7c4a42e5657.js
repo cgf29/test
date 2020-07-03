603edb103b952bb845cadb913c9ea8298457a6d7
Your Name <vanyasv@gmail.com>
2020-07-03 12:55:20 +0300

    Updates

---
+++ precache-manifest.c70312f999c37440a5f5a7c4a42e5657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e977681ab6e90dd4178d75a58062cb28",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "beeec6e69cdfb0a51582",
+    "url": "/test/static/css/main.714b8669.chunk.css"
+  },
+  {
+    "revision": "29e06fb48e6aaa11d049",
+    "url": "/test/static/js/2.6c2959af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.6c2959af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beeec6e69cdfb0a51582",
+    "url": "/test/static/js/main.729a1bf7.chunk.js"
+  },
+  {
+    "revision": "c0c691af2b5cd6ed53d1",
+    "url": "/test/static/js/runtime-main.88497f4c.js"
+  }
+]);
